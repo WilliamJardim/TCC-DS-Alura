c5841edd1de826f8b935d2fc4864226c424cfce2
William Jardim <williamalvesjardim@gmail.com>
2025-03-22 10:29:45 -0300

    Novos graficos

---
+++ analise-exporatoria/frequencias/media-pragas-por-estacao.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Carregar o dataset
+dataset = pd.read_csv('../datasets/dataset-agricultura-v5-balanceado.csv', sep=';')
+
+media_pragas = dataset.groupby('Estacao_Ano')['Num_Praga'].mean()
+print(media_pragas)
+
+plt.figure(figsize=(8, 6))
+media_pragas.plot(kind='bar', color='blue')
+plt.xlabel('Estação')
+plt.ylabel('Média Pragas')
+plt.title('Média de Pragas das Plantas por Estação')
+plt.xticks(rotation=0)
+plt.show()
